fbb1686b746ec4d35df256703222c657bccab082
giris <giris@ROCKY>
2025-07-27 11:33:07 +0530

    Add project files.

---
+++ MouseHover.py
@@ -0,0 +1,13 @@
+import time
+from requests import get
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common import service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+driver= webdriver.Chrome(service = Service(ChromeDriverManager().install()))
+
+driver.get("file:///C:/Users/giris/source/repos/nseDemoUemyPythonProject/mouseHover.html")
+hoverable = driver.find_element(By.XPATH, "/html/body/div[17]")
+webdriver.ActionChains(driver).move_to_element(hoverable).perform()
+time.sleep(10)  # Wait for a while to see the result
