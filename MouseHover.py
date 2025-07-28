29411c283880a619fb7fbe8f27a9846239ed2a9e
vampire010 <girishrathode.girish@gmail.com>
2025-07-28 11:18:17 +0530

    28 July Code

--- MouseHover.py
+++ MouseHover.py
@@ -7,7 +7,9 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 driver= webdriver.Chrome(service = Service(ChromeDriverManager().install()))
 
-driver.get("file:///C:/Users/giris/source/repos/nseDemoUemyPythonProject/mouseHover.html")
+driver.get("file:///C:/Users/giris/source/repos/nseDemoUemyPythonProject/htmlCode/mouseHover.html")
+driver.implicitly_wait(10)  # Set implicit wait for 10 seconds
+time.sleep(5)  # Wait for a while to see the result
 hoverable = driver.find_element(By.XPATH, "/html/body/div[17]")
 webdriver.ActionChains(driver).move_to_element(hoverable).perform()
-time.sleep(10)  # Wait for a while to see the result
+time.sleep(5)  # Wait for a while to see the result
